0998731f985d5a208ccdcda959af39f854be07df
Buringskul <85359963+Buringskul@users.noreply.github.com>
2024-02-22 11:47:38 -0500

    Time: 19 ms (19.83%), Space: 27.7 MB (5.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,20 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        int answer;
-
-        for (auto i : nums) {
-            freq[i]++;
-        }
-
-        for (auto i : freq) {
-            if (i.second > (nums.size() / 2)) {
-                answer = i.first;
-                break;
-            }
-        }
-
-        return answer;
+        sort(nums.begin(), nums.end());
+        int middle = nums.size() / 2;
+        return nums[middle];
     }
 };
