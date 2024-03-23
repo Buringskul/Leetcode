39073dc20e08d231d1f7fb5cd95213bf855dce79
Buringskul <85359963+Buringskul@users.noreply.github.com>
2024-03-23 13:55:33 -0400

    Time: 173 ms (31.43%), Space: 23.8 MB (74.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,14 +3,13 @@ class Solution:
         ans = [1] * len(nums)
         left_product = 1
         right_product = 1
-
+        
         for i in range(len(nums)):
             ans[i] *= left_product
             left_product *= nums[i]
-        
+            
         for i in range(len(nums) - 1, -1, -1):
             ans[i] *= right_product
             right_product *= nums[i]
-            
-        return ans
         
+        return ans
