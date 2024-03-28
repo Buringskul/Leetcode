128926ca858a32ef0f3445216e4f04216d45dcc7
Buringskul <85359963+Buringskul@users.noreply.github.com>
2024-03-28 15:39:58 -0400

    Time: 98 ms (72.61%), Space: 18.5 MB (39.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -2,42 +2,25 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         if len(height) <= 2:
             return 0
-        
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = 0, 0
         answer = 0
-        max_length = 0
-        max_area = [0] * len(height) 
-        
-        for i in range(len(height)):
-            if height[i] != 0:
-                left = i
-                break
-        
-        for i in range(len(height) - 1, -1, -1):
-            if height[i] != 0:
-                right = i
-                break
 
         while left < right:
-            if height[left] >= height[right]:
-                length = height[right]
-                right -= 1
-            else:
-                length = height[left]
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    answer += left_max - height[left]
                 left += 1
-
-            if length > max_length:
-                for i in range(left, right + 1):
-                    max_area[i] = length
-            max_length = max(max_length, length)
-        
-        for i in range(len(height) - 1):
-            if height[i] >= max_area[i]:
-                max_area[i] = 0
             else:
-                max_area[i] = max_area[i] - height[i]
-                
-            answer += max_area[i]
-             
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    answer += right_max - height[right]
+                right -= 1
+
         return answer
         
 
