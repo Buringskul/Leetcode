dd403963c3cfeb40dcf6f84d2a27dc6b21e82acc
Buringskul <85359963+Buringskul@users.noreply.github.com>
2024-03-28 15:32:39 -0400

    Time: 3287 ms (5.52%), Space: 18.7 MB (16.73%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -17,38 +17,17 @@ class Solution:
                 right = i
                 break
 
-        while left < right:    
-            if height[left] > height[right]:
+        while left < right:
+            if height[left] >= height[right]:
                 length = height[right]
-                
-                if length > max_length:
-                    for i in range(left, right):
-                        max_area[i] = 0
-                        max_area[i] += length
-                        
                 right -= 1
-                
-            elif height[left] == height[right]:
-                length = height[left]
-                
-                if length > max_length:
-                    for i in range(left, right):
-                        max_area[i] = 0
-                        max_area[i] += length
-                        
-                right -= 1
-                left += 1
-                      
             else:
                 length = height[left]
-                
-                if length > max_length:
-                    for i in range(left, right):
-                        max_area[i] = 0
-                        max_area[i] += length 
-                        
                 left += 1
-            
+
+            if length > max_length:
+                for i in range(left, right + 1):
+                    max_area[i] = length
             max_length = max(max_length, length)
         
         for i in range(len(height) - 1):
@@ -58,8 +37,7 @@ class Solution:
                 max_area[i] = max_area[i] - height[i]
                 
             answer += max_area[i]
-            
-        print(max_area)    
+             
         return answer
         
 
